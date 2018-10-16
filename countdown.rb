5616a225ce2652314a336dccf9f9c1bc2431954f
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-16 00:05:59 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
-
+integer == 5
 def countdown
+  if integer == 0 
+    puts "HAPPY NEW YEAR!"
+  end
+  while integer != 0 
+    puts "#{integer} SECOND(S)!"
+  end
+  integer -= 1
 end
